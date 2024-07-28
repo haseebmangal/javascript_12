91d4240b570c2f99bfa375cb40309e3338344127
Abdul haseeb <haseebmangal@yahoo.com>
2024-07-28 15:17:57 +0500

    solved some problems in JavaScript

---
+++ exercise_task/index.js
@@ -0,0 +1,30 @@
+// javascript exercise problems
+
+// problem 01
+// Task: Declare a variable str and assign it the value "Welcome to JavaScript!". Print the length of the string.
+// var str = "welcome to JavaScript";
+// console.log("The length of the string is : " + str.length);
+
+// problem 02
+// Task: Declare a variable str and assign it the value "Programming is awesome". Extract and print the word "awesome" from the string.
+// var str = "Programming is awesome";
+// var extract = str.slice(str.indexOf("awesome"));
+// console.log(extract);
+
+// Problem 03
+// Task: Declare a variable str and assign it the value "Hello, Universe!". Extract and print the first five characters of the string.
+// var str = "Hello, Universe!";
+// var chr = str.slice(0, 5);
+// console.log(chr);
+
+// Problem 04
+// Task: Declare a variable str and assign it the value "Full Stack Developer". Extract and print the substring "Stack".
+// var str = "Full Stack Developer";
+// var sub = str.slice(5, 10);
+// console.log(sub);
+
+// Problem 05
+// Task: Declare a variable str and assign it the value "Javascript Strings" Extract and print the last four characters of the string.
+var str ="Javascript Strings";
+var ext = str.slice(-4);
+console.log(ext);
